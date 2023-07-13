7b20fc4f19caa3c0dc934806ce31956cc47b8712
Viktor Sadykov <vityasadykov@gmail.com>
2023-07-13 19:34:02 +0700

    Add project files.

---
+++ WebApplication2/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
